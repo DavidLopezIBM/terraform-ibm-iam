718ac8aca1b4fadc47b61fe8be9ffbe04159e330
DavidLopezIBM <58993592+DavidLopezIBM@users.noreply.github.com>
2021-02-22 10:47:09 -0500

    Update versions.tf

--- examples/access-group/versions.tf
+++ examples/access-group/versions.tf
@@ -4,9 +4,11 @@
 #####################################################
 
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     ibm = {
-      source = "IBM-Cloud/ibm"
+      source  = "IBM-Cloud/ibm"
+      version = "1.19.0"
     }
   }
 }
